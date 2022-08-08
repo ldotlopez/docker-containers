7badbfecddd18157859b66642f69e8afec3fd95b
Luis López <ldotlopez@gmail.com>
2022-08-08 15:32:06 +0200

    Fix stuff in VLC image

--- vlc/entrypoint.sh
+++ vlc/entrypoint.sh
@@ -1,30 +1,62 @@
 #!/bin/bash
+
 set -euo pipefail
 IFS=$'\n\t'
 
-USER_USER="abc"
-USER_GROUP="abc"
+PROCESS_USER="abc"
+PROCESS_GROUP="abc"
+APP_DIR="/app"
 
-USER_UID="${PUID:-1000}"
-USER_GID="${PGID:-1000}"
+PUID="${PUID:-1000}"
+PGID="${PGID:-1000}"
+
+ALSA_SCONTROL="${ALSA_SCONTROL:-Master}"
+ALSA_DEVICE="${ALSA_DEVICE:-default}"
+ALSA_PORTS="${ALSA_PORTS:-PCM}"
 
 VLC_PORT="${VLC_PORT:-4212}"
 VLC_PASSWORD="${VLC_PASSWORD:-password}"
 
-if [ ! -e /.configured ]; then
-   echo "*** Configuring container ***"
+echo "*** Configuring container ***"
 
-   addgroup -g "$USER_GID" "$USER_GROUP"
-   adduser -u "$USER_UID" -h / -g '' -G "$USER_GROUP" -D -H "$USER_USER"
+if [ ! -e "$APP_DIR/.configured" ]; then
+    echo "*** Configuring user/group: ${PROCESS_USER}:${PROCESS_GROUP} (${PUID}:${PGID}) ***"
+    addgroup -g "$PGID" "$PROCESS_GROUP"
+    adduser -u "$PUID" -h "$APP_DIR" -g '' -G "$PROCESS_GROUP" -D -H "$PROCESS_USER"
 
-   addgroup -g "$(stat -c %g /dev/snd/timer)" -S alsa
-   adduser "$USER_USER" alsa
+    addgroup -g "$(stat -c %g /dev/snd/timer)" -S alsa
+    adduser "$PROCESS_USER" alsa
 
-   touch "/.configured"
+    mkdir -p "$APP_DIR"
 fi
 
-USER_UID="$(id -u $USER_USER)"
-USER_GID="$(id -g $USER_USER)"
+chown -R "$PUID:$PGID" "$APP_DIR"
+touch "$APP_DIR/.configured"
 
-echo "*** Starting vlc as user: ${USER_USER} (uid:${USER_UID}/gid:${USER_GID}) ***"
-su -l - "$USER_USER" -c "vlc-wrapper -I telnet --telnet-host=127.0.0.1 --telnet-port '$VLC_PORT' --telnet-password '$VLC_PASSWORD'"
+echo "*** Configuring XDG environment ***"
+export XDG_CACHE_DIR="/tmp/cache"
+export XDG_CONFIG_DIR="$APP_DIR/config"
+export XDG_DATA_DIR="$APP_DIR/data"
+mkdir -p "$XDG_CACHE_DIR" "$XDG_CONFIG_DIR" "$XDG_DATA_DIR"
+
+echo "*** Configuring ALSA ***"
+if [[ -n "$ALSA_DEVICE" && -n "$ALSA_SCONTROL" ]]; then
+    amixer -q -D "$ALSA_DEVICE" sset "$ALSA_SCONTROL" 100%
+    amixer -q -D "$ALSA_DEVICE" sset "$ALSA_SCONTROL" unmute
+fi
+
+if [[ -n "$ALSA_DEVICE" && "$ALSA_PORTS" ]]; then
+    for PORT in $(echo "$ALSA_PORTS" | tr , " "); do
+        amixer -q -D "$ALSA_DEVICE" sset "$PORT" 100%
+        amixer -q -D "$ALSA_DEVICE" sset "$PORT" unmute
+    done
+fi
+
+echo "*** Starting VLC ***"
+exec sudo --set-home \
+    --preserve-env=XDG_CACHE_DIR,XDG_CONFIG_DIR,XDG_DATA_DIR \
+    --user "$PROCESS_USER" \
+    vlc -vvvv \
+    --no-dbus \
+    -A alsa --alsa-audio-device "$ALSA_DEVICE" \
+    -I telnet --telnet-host="127.0.0.1" --telnet-port "$VLC_PORT" --telnet-password "$VLC_PASSWORD"
