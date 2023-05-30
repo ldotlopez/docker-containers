3338df62d9d6fe5d5390a5af4cc93c23e8e4cc94
L. López <ldotlopez@gmail.com>
2023-05-30 14:02:13 +0200

    environment wars

--- agate/build/entrypoint.sh
+++ agate/build/entrypoint.sh
@@ -5,10 +5,10 @@ IFS=$'\n\t'
 
 PUID="${PUID:-0}"
 PGID="${PGID:-0}"
-HOSTNAME="${HOSTNAME:-localhost}"
-LANG="${LANG:-en-US}"
-CONTENT="/content"
-CERTIFICATES="${CERTIFICATES:-${CONTENT}/.certificates}"
+AGATE_HOSTNAME="${AGATE_HOSTNAME:-localhost}"
+AGATE_LANG="${AGATE_LANG:-en-US}"
+AGATE_CONTENT="/content"
+AGATE_CERTIFICATES="${CERTIFICATES:-${AGATE_CONTENT}/.certificates}"
 
 #
 # Setup process support
@@ -25,24 +25,27 @@ USERNAME="$( (getent passwd "$PUID" 2>/dev/null || true) | cut -d ':' -f 1)"
 if [[ -z "$USERNAME" ]]; then
 	USERNAME="abc"
 	echo "[*] Creating user '$USERNAME' with UID $PUID"
-	adduser -G "$GROUPNAME" -u "$PUID" -H -D -h "$CONTENT" "$USERNAME"
+	adduser -G "$GROUPNAME" -u "$PUID" -H -D -h "$AGATE_CONTENT" "$USERNAME"
 fi
 
 #
 # Setup agate stuff
 #
 
-[[ -d "$CONTENT" ]] || mkdir "$CONTENT"
-[[ -d "$CERTIFICATES" ]] || mkdir "$CERTIFICATES"
-sudo chown -R "$PUID:$PGID" "$CONTENT" "$CERTIFICATES"
+[[ -d "$AGATE_CONTENT" ]] || mkdir "$AGATE_CONTENT"
+[[ -d "$AGATE_CERTIFICATES" ]] || mkdir "$AGATE_CERTIFICATES"
+sudo chown -R "$PUID:$PGID" "$AGATE_CONTENT" "$AGATE_CERTIFICATES"
 
-if [[ ! -f "$CONTENT/index.gmi" ]]; then
-	echo "[*] Creating index.gmi"
-	cat >"$CONTENT/index.gmi" <<-EOF
-		# Hello
+if [[ ! -f "$AGATE_CONTENT/index.gmi" ]]; then
+	cp -r /usr/share/agate/content/* "$AGATE_CONTENT/"
+	chown -R "$PUID:$PGID" "$AGATE_CONTENT"
 
-		This is "$HOSTNAME"
-	EOF
+	# echo "[*] Creating index.gmi"
+	# cat >"$CONTENT/index.gmi" <<-EOF
+	# 	# Hello
+	#
+	# 	This is "$HOSTNAME"
+	# EOF
 fi
 
 #
@@ -51,17 +54,17 @@ fi
 
 echo "[*] Running agate process: '$USERNAME:$GROUPNAME' ($PUID:$PGID)"
 echo "[*] Running agate addr: '0.0.0.0:1965' and '[::]:1965'"
-echo "[*] Running agate content: '$CONTENT'"
-echo "[*] Running agate certificates: '$CERTIFICATES'"
-echo "[*] Running agate hostname: '$HOSTNAME'"
-echo "[*] Running agate language: '$LANG'"
+echo "[*] Running agate content: '$AGATE_CONTENT'"
+echo "[*] Running agate certificates: '$AGATE_CERTIFICATES'"
+echo "[*] Running agate hostname: '$AGATE_HOSTNAME'"
+echo "[*] Running agate language: '$AGATE_LANG'"
 exec sudo \
 	--user="$USERNAME" \
 	--group="$(getent group "$PUID" | cut -d : -f 1)" \
 	/usr/bin/agate \
 	--addr '0.0.0.0:1965' \
 	--addr '[::]:1965' \
-	--content "$CONTENT" \
-	--certs "$CERTIFICATES" \
-	--hostname "$HOSTNAME" \
-	--lang "$LANG"
+	--content "$AGATE_CONTENT" \
+	--certs "$AGATE_CERTIFICATES" \
+	--hostname "$AGATE_HOSTNAME" \
+	--lang "$AGATE_LANG"
