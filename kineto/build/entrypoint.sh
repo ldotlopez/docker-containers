8311badff806512e55a47de6db90d44767d94e3f
L. López <ldotlopez@gmail.com>
2025-08-27 12:31:43 +0200

    Minor improvements for kineto

--- kineto/build/entrypoint.sh
+++ kineto/build/entrypoint.sh
@@ -3,19 +3,30 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+#
+# Process ownership
+#
+
 PUID="${PUID:-0}"
 PGID="${PGID:-0}"
 
-GEMINI_URL="${GEMINI_URL:-gemini://localhost}"
-CSS_FILEPATH="${CSS_FILEPATH:-}"
-
-if ! (getent group "$PGID" 1>/dev/null 2>/dev/null); then
-	addgroup -g "$PGID" kineto
+GROUPNAME="$( (getent group "$PGID" 2>/dev/null || true) | cut -d ':' -f 1)"
+if [[ -z "$GROUPNAME" ]]; then
+	GROUPNAME="abc"
+	echo "[*] Creating group '$GROUPNAME' with GID $PGID"
+	addgroup -g "$PGID" "$GROUPNAME"
 fi
 
-if ! (getent passwd "$PUID" 1>/dev/null 2>/dev/null); then
-	adduser -G kineto -u 1000 -H -D agate
+USERNAME="$( (getent passwd "$PUID" 2>/dev/null || true) | cut -d ':' -f 1)"
+if [[ -z "$USERNAME" ]]; then
+	USERNAME="abc"
+	echo "[*] Creating user '$USERNAME' with UID $PUID"
+	adduser -G "$GROUPNAME" -u "$PUID" -H -D -h "$AGATE_CONTENT" "$USERNAME"
 fi
+
+#
+# Run kineto
+#
 
 CMDL="/usr/bin/kineto"
 CMDL+=("-b" "0.0.0.0:80")
@@ -24,8 +35,13 @@ if [[ -n "$CSS_FILEPATH" ]]; then
 fi
 CMDL+=("$GEMINI_URL")
 
+echo "[*] Running as: '$USERNAME:$GROUPNAME' ($PUID:$PGID)"
+echo "[*]         bind: '0.0.0.0:80'"
+echo "[*]         gemini url: '${GEMINI_URL}'"
+echo "[*]         css file: '${CSS_FILEPATH:-(none)}'"
+
 # kineto [-b 127.0.0.1:8080] [-s style.css] [-e style.css] gemini://example.org
 exec sudo \
-	--user="$(getent passwd "$PUID" | cut -d : -f 1)" \
-	--group="$(getent group "$PUID" | cut -d : -f 1)" \
+	--user="$USERNAME" \
+	--group="$GROUPNAME" \
 	"${CMDL[@]}"
